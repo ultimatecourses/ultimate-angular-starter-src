61593414be3ae0f5d011eca033884cded5731e3f
Todd Motto <todd@toddmotto.com>
2016-06-18 23:08:55 +0100

    fix(stretch.directive): Add 'px'

--- 24-behaviour-directives/content/js/stretch.directive.js
+++ 24-behaviour-directives/content/js/stretch.directive.js
@@ -4,10 +4,10 @@ function stretch() {
     link: function ($scope, $element, $attrs) {
       var element = $element[0];
       element.addEventListener('focus', function () {
-        this.style.width = 250;
+        this.style.width = '250px';
       });
       element.addEventListener('blur', function () {
-        this.style.width = 120;
+        this.style.width = '120px';
       });
     }
   };
